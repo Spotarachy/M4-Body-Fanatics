badd61585b69c2bf178fd06146db8c12e2fe9c0c
Spotarachy <wilson.f.mario@gmail.com>
2022-04-18 12:30:19 +0000

    added new categories  List

--- products/models.py
+++ products/models.py
@@ -12,7 +12,6 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
-
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
@@ -21,6 +20,8 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+    colour = models.ImageField(null=True, blank=True)
 
 
     def __str__(self):
