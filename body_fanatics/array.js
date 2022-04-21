96ea0b0c0a973512bd4968189556cf430d4b39a9
Spotarachy <wilson.f.mario@gmail.com>
2022-04-21 16:26:06 +0000

    loading products next

---
+++ body_fanatics/array.js
@@ -0,0 +1,9 @@
+
+
+// PImage[] apparel = new PImage[3];
+
+// void setup() {
+//     size(500,600);
+//     image = loadImage("a.jpeg");
+//     image = loadImage("b.jpeg");
+// }
--- body_fanatics/urls.py
+++ body_fanatics/urls.py
@@ -23,5 +23,3 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
