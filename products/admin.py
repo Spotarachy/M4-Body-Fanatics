1f63b6cc26fe72db0a8107c571953ce9634a9c70
Spotarachy <wilson.f.mario@gmail.com>
2022-04-25 18:45:58 +0000

    added product page

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'sku',
+#         'name',
+#         'description',
+#         'price',
+#         'category',
+#         'image',
+#         'colour',
+#         'sale_type',
+#     )
+#     ordering = ('sku')
+
+# class CategoriesAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'friendly_name',
+#         'name',
+#     )
+
 admin.site.register(Product)
 admin.site.register(Category)
 
--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=245, null=True, blank=True)
 
@@ -22,7 +26,6 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     colour = models.ImageField(null=True, blank=True)
-
 
     def __str__(self):
         return self.name
