1ee1ae3f3de3eff0357ac760bcf81c99163b6ec2
Spotarachy <wilson.f.mario@gmail.com>
2022-04-26 20:51:12 +0000

    solve the major bug

--- products/models.py
+++ products/models.py
@@ -5,8 +5,8 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
-    name = models.CharField(max_length=600)
-    friendly_name = models.CharField(max_length=600, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
     
     def __str__(self):
         return self.name
@@ -15,9 +15,10 @@ class Category(models.Model):
         return self.friendly_name
 
 class Product(models.Model):
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=600, null=True, blank=True)
-    name = models.CharField(max_length=600)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -28,7 +29,6 @@ class Product(models.Model):
     sale_type = models.TextField(null=True, blank=True)
     friendly_name = models.TextField(null=True, blank=True)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    
 
     def __str__(self):
         return self.name
