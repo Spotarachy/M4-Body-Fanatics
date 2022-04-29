e8e6bdab5e37dbf21892dfaff929531db7facbbd
Spotarachy <wilson.f.mario@gmail.com>
2022-04-29 11:52:28 +0000

    link Main page to product.id

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
@@ -11,3 +11,12 @@ def all_products(request):
     }   
     return render(request, 'products/products.html', context)
 
+def products_detail(request):
+    """This view will get specific product information """
+
+    products = get_objects_or_404(Products, pk=product_id)
+
+    context = {
+        'product': product,
+    }   
+    return render(request, 'products/products.html', context)
