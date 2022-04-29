93cb08b60f5977cf9bb1c2fb694f65fbd85301fc
Spotarachy <wilson.f.mario@gmail.com>
2022-04-29 14:16:00 +0000

    link search trem  to products

--- products/views.py
+++ products/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse,  get_object_or_404 
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 def all_products(request):
     """This view will show our products page & it will include both the search &sorting """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search was Sent!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term':query,
     }   
     return render(request, 'products/products.html', context)
 
