94d17a56d5a7e8c642fccc0fb052b0e2c549a217
Spotarachy <wilson.f.mario@gmail.com>
2022-04-29 04:37:54 +0000

    Customized the admin

--- products/admin.py
+++ products/admin.py
@@ -3,24 +3,29 @@ from .models import Product, Category
 
 # Register your models here.
 
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'sku',
-#         'name',
-#         'description',
-#         'price',
-#         'category',
-#         'image',
-#         'colour',
-#         'sale_type',
-#     )
-#     ordering = ('sku')
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'description',
+        'price',
+        'category',
+        'image',
+        'colour',
+        'sale_type',
+    )
+    ordering = ('-sku',)
 
-# class CategoriesAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'friendly_name',
-#         'name',
-#     )
+#the order can be chang is using the name or not sku!
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
 
 admin.site.register(Product)
 admin.site.register(Category)
+
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Category, CategoriesAdmin)
