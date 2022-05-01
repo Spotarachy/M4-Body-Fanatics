beb493d6509065fba0fa3ecd2fbf7bd336fdd15a
Spotarachy <wilson.f.mario@gmail.com>
2022-05-01 19:20:54 +0000

    added category link to Filter

--- products/views.py
+++ products/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, redirect, reverse,  get_object_or_404 
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
     """This view will show our products page & it will include both the search &sorting """
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = categories.objects.filter(name__in=categories)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -21,8 +28,9 @@ def all_products(request):
 
     context = {
         'products': products,
-        'search_term':query,
-    }   
+        'search_term': query,
+        'current_categories': categories,
+            }   
     return render(request, 'products/products.html', context)
 
 def products_detail(request):
