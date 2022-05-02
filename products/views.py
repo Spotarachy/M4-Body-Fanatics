9cfb43eaefa1bb53b2b4a455a3727fb22703e218
Spotarachy <wilson.f.mario@gmail.com>
2022-05-02 19:02:59 +0000

    sorting sterling complete

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse,  get_object_or_404 
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q
+
 from .models import Product, Category
 
 def all_products(request):
