c4ae6c42cafa0f190de4029845be1ea487982230
Spotarachy <wilson.f.mario@gmail.com>
2022-05-03 18:43:50 +0000

    added counter to bag

--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
-    """this will ring customers to their shopping bag, (Home) page"""
+    """ this will ring customers to their shopping bag, (Home) page """
     
     return render(request, 'bag/bag.html')
+
+def add_to_bad(request, item_id):
+    """ how item are added to bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.GET('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity 
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url) 
