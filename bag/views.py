f33bfb3692403f6d66475e95bc31912bd4a2cd60
Spotarachy <wilson.f.mario@gmail.com>
2022-05-13 20:03:15 +0000

    product bag GETitem_id working great

--- bag/views.py
+++ bag/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render, redirect
 
 def view_bag(request):
     """ this will ring customers to their shopping bag, (Home) page """
-    
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
@@ -14,22 +13,23 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
-        size = request.POST['size']
-    bag = request.session.get('bag', {})
-    
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {}) 
+
     if size:
         if item_id in list(bag.keys()):
-            if size in bag[item_id]['item_by_size'].keys()
-                bag[item_id]['item_by_size'].keys() += quantity 
+            if size in bag[item_id]['item_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity 
             else:
-                bag[item_id]['item_by_size'].keys() = quantity 
+                bag[item_id]['items_by_size'][size] = quantity 
         else:
-            bag[item_id] = {'item_by_size'}: {size: quantity}}
-    if item_id in list(bag.keys)):
-        bag[item_id] += quantity
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] += quantity
-
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] += quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url) 
+
