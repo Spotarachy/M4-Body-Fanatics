02843d5f6cc6fcd51e3b98d0eaee5a5a1f71e11c
Spotarachy <wilson.f.mario@gmail.com>
2022-05-17 18:17:06 +0000

    created a check out app in the shopping bag

--- checkout/models.py
+++ checkout/models.py
@@ -9,6 +9,7 @@ from products.models import Product
 # copy of the order layout from the completed project!!
 
 class Order(models.Model):
+
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -22,17 +23,43 @@ class Order(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True) # willnot be needed!
 
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+def _generate_order_number(self):
+
+    return uuid.uuid4().hex.upper()
+
+def update_total(self):
+    self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+    if self.order_total <settings.FREE_DELIVERY_THRESHOLD:
+        self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+    else:
+        self.delivery_cost = 0
+    self.grand_total = self.order_total + self.delivery_cost
+    self.save()
+
+def save(self, *args, **kwargs):
+    if not self.order_number:
+        self.order_number = self._generate_order_number()
+    super().save(*args, **kwargs)
+
+def __str__(self):
+    return self.order_number
+
 class OrderLineItem(models.Model):
-    Order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE, related_name='lineitems')
 
+    Order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE,related_name='lineitems')
     product = models.ForeignKey(Product, null=False, on_delete=models.CASCADE)
-
-    product_size = models.CharField(max_length=2, null=True, blank=True) # SX, S, M, L, XL
-
+    product_size = models.CharField(max_length=2, null=True, blank=True) # SX,S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,null=False, blank=False, editable=False)
+
+def save(self, *args, **kwargs):
+
+    self.lineitem_total = self.product.price * self.quantity
+    super().save(*args, **kwargs)
+
+def __str__(self):
+    return f'SKU {self.product.sku} on order {self.order.order_number}'
