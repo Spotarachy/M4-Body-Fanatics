a25bd3303fb02d3631919c34ab1c4ea2ffcbde69
Spotarachy <wilson.f.mario@gmail.com>
2022-05-27 11:16:18 +0000

    addad Stipe  payment methods for checkout

---
+++ checkout/js/stripe.js
@@ -0,0 +1,27 @@
+/* 
+Js Copied from completed project.
+CSS is from: https://stripe.com/docs/stripe-js 
+
+*/
+
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#555',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
--- checkout/views.py
+++ checkout/views.py
@@ -9,10 +9,13 @@ def checkout(request):
     if not bag:
         messages.error(request, 'Shoping bag is empty')
         return redirect(reverse('products'))
-    order_from = OrderForm()
+
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_from,
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L3yfhH1FgX9COuGJP1j9lYeMGEJPxUqB8iau8noRZymJ8CjjZysSMxWBU6ARZ2NvLJaAfwifpnyjba1k64MlcvU004EX1Lbgz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
