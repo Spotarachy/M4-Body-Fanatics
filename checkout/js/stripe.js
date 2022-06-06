1092c1488289f930766fe035b458363d67335a94
Spotarachy <wilson.f.mario@gmail.com>
2022-06-06 22:13:35 +0000

    Added checkout form data caching in payment intent

--- checkout/js/stripe.js
+++ checkout/js/stripe.js
@@ -4,13 +4,13 @@ CSS is from: https://stripe.com/docs/stripe-js
 
 */
 
-var stripePublickey = $('#id_stripe_public_key').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublickey);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
-        color: '#555',
+        color: '#000',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
@@ -45,33 +45,74 @@ card.addEventListener('change', function (event){
 // taken from Stripe, this well Handle form submit
 var form = document.getElementById('payment-form');
 
+
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({'disabled': true});
     $('#submit-button').attr('disabled', true);
     $('#payment-from').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-from').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // from using {% csrf_token %} in the from
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postcode = {
+    'csrfmiddlewaretoken': csrfToken,
+    'client_secret': clientSecret,
+    'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.full_name.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.full_name.value),
+                    }
+                }
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-from').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    });
+        });
+    }).fail(function () {
+        // Reload the page, the error will be in django messages
+        location.reload();
+    })
 });
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,25 +2,34 @@ from django.http import HttpResponse
 
 
 class StripeWH_Handler:
-    """ Handle Stripe webhooks """
-
+    """
+    Handle Stripe webhooks 
+    """
     def __init__(self, request):
         self.request = request
    
     def handle_event(self, event):
-        """ Handle a generic/unknown/unexpected webhook event"""
+        """ 
+        Handle a generic/unknown/unexpected webhook event
+        """
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
-        """ Handle a payment_intent_succeeded webhook event"""
+        """
+        Handle a payment_intent_succeeded webhook event
+        """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_failed(self, event):
-        """ Handle a payment_intent_failed webhook event"""
+        """ 
+        Handle a payment_intent_failed webhook event
+        """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
