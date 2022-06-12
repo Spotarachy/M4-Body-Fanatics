8b311dfa3c182a5743f473b57256eac5b7423ae3
Spotarachy <wilson.f.mario@gmail.com>
2022-06-12 22:18:31 +0000

    Added main functionality of the profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404 
 
 from .models import UserProfile
+from .forms import UserProfileFrom
 
 
 def profile(request):
@@ -9,9 +10,14 @@ def profile(request):
     """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+
+    form = UserProfileFrom(isinstance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'from': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
