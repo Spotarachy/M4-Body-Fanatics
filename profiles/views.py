4cc3c2cd52271d50cecc7b351fb8849d01942582
Spotarachy <wilson.f.mario@gmail.com>
2022-06-13 20:23:49 +0000

    Added profile form, to views

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404 
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileFrom
@@ -10,6 +11,11 @@ def profile(request):
     """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileFrom(request.POST, instance=profile)
+        if form.is_valid():
+            if form.save():
+            messages.success(request, 'Profile updated successfully')
 
     form = UserProfileFrom(isinstance=profile)
     orders = profile.orders.all()
@@ -18,6 +24,7 @@ def profile(request):
     context = {
         'from': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
