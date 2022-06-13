c66c0acc181480d108143696fe726893c0df8fe1
Spotarachy <wilson.f.mario@gmail.com>
2022-06-13 23:23:01 +0000

    profiles completed & confirmation emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -13,7 +16,28 @@ class StripeWH_Handler:
     """
     def __init__(self, request):
         self.request = request
-   
+    def _send_confirmation_email(self, order):
+        """
+        Sending the uesd a confirmation email
+        """
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/email_subject.txt',
+            {'order': order})
+
+        body = render_to_string(
+            'checkout/confirmation_emails/email_body.txt',
+            {'order': order, 'contact_email': settings.default_FROM_EMAIL})
+        
+        send_mail(
+            subject, 
+            body, 
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+            )
+
+
+        self.request = request
     def handle_event(self, event):
         """ 
         Handle a generic/unknown/unexpected webhook event
@@ -78,6 +102,7 @@ class StripeWH_Handler:
                 attempt +=1
                 time.sleep(1)
         if order_exists:
+            self ._send_confirmation_email(order)
             return HttpResponse(
                     content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database', status=200)
         else:
@@ -120,6 +145,7 @@ class StripeWH_Handler:
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',status=500)
+        self ._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
