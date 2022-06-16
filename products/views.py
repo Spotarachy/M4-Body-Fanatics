9f8b523f801bc04b249c88db9e4a5eca0d8aa440
Spotarachy <wilson.f.mario@gmail.com>
2022-06-16 22:55:07 +0000

    Product Admin -Deleting items

--- products/views.py
+++ products/views.py
@@ -78,8 +78,9 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            product = form.save()
             messages.success(request,'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -116,3 +117,9 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+def delete_product(request, template, context)
+
+    product = get_object_or_404(Product,  pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted!')
+    return redirect(reverse('products'))
