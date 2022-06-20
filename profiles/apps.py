1e9caed659f5bcea58d6180e3ce46a62c4eb489d
Spotarachy <wilson.f.mario@gmail.com>
2022-06-20 08:07:16 +0000

    added  missing docsting

--- profiles/apps.py
+++ profiles/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
-
+"""
+Provides some arithmetic functions
+"""
 
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
--- team/apps.py
+++ team/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
-
+"""
+Provides some arithmetic functions
+"""
 
 class TeamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
