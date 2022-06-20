99bb4d2d820be35aa35aa5060136776aaa33afde
Spotarachy <wilson.f.mario@gmail.com>
2022-06-20 19:32:46 +0000

    removed secret key set up

--- body_fanatics/settings.py
+++ body_fanatics/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4_#q_ttmca+1a%f^m5w(2*v2x+dqw2=ce7i&+idx16kk#(5o+z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+# 'django-insecure-4_#q_ttmca+1a%f^m5w(2*v2x+dqw2=ce7i&+idx16kk#(5o+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['m-four-body-fanatics.herokuapp.com', 'localhost']
 
