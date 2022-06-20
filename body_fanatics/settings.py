7732b52cecd60de76baa9e38ca4e61b5ce5600ec
Spotarachy <wilson.f.mario@gmail.com>
2022-06-20 11:16:17 +0000

    reinstall requirements and database for heroku

--- body_fanatics/settings.py
+++ body_fanatics/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,7 +125,7 @@ WSGI_APPLICATION = 'body_fanatics.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
